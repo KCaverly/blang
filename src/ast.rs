1d6c98e18ce7ef2ee23745255219b8c99dec79e2
KCaverly <kylebcaverly@gmail.com>
2023-03-21 15:04:18 -0400

    progress on ast

--- src/ast.rs
+++ src/ast.rs
@@ -1,5 +1,5 @@
 use crate::lexer::Lexer;
-use crate::token::Token;
+use crate::token::{Token, TokenType};
 
 trait Node {
     fn token_literal(&self) -> Option<String>;
@@ -62,7 +62,7 @@ struct Parser {
 }
 
 impl Parser {
-    fn new(&self, lexer: Lexer) -> Parser {
+    fn new(lexer: Lexer) -> Parser {
         let mut p = Parser {
             lexer,
             current_token: None,
@@ -78,5 +78,69 @@ impl Parser {
     fn next_token(&mut self) {
         self.current_token = self.peek_token.clone();
         self.peek_token = Some(self.lexer.next_token());
+    }
+
+    fn parse(&mut self) -> Program {
+        println!("Current Token: {:?}", self.current_token);
+        println!("Peek Token: {:?}", self.peek_token);
+
+        // Create a Blank Program to Start
+        let mut program = Program { statements: vec![] };
+
+        // Iterate through all tokens in the Lexer
+        while self.current_token.clone().unwrap().token_type != TokenType::EOF {
+            let statement = self.parse_statement();
+            program.statements.push(statement);
+
+            self.next_token();
+        }
+
+        return program;
+    }
+
+    fn parse_statement(&self) -> Box<dyn Statement> {
+        return Box::new(self.parse_let_statement());
+    }
+
+    fn parse_let_statement(&self) -> LetStatement {
+        return LetStatement {
+            token: self.current_token.clone().unwrap(),
+            name: Identifier {
+                token: self.peek_token.clone().unwrap(),
+                value: "".to_string(),
+            },
+            value: Box::new(Identifier {
+                token: self.peek_token.clone().unwrap(),
+                value: "".to_string(),
+            }),
+        };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_simple_parser() {
+        let test_string = r#"let x = 5;
+        let y = 10;
+        let foobar = 838383;"#;
+
+        let lexer = Lexer::new(test_string.to_string());
+        let mut parser = Parser::new(lexer);
+
+        let program = parser.parse();
+
+        for statement in &program.statements {
+            println!("{:?}", statement.token_literal());
+        }
+
+        assert!(
+            program.statements.len() == 3,
+            "Statements Returned Does not Equal 3"
+        );
+
+        panic!("HOW IS THIS WORKING!");
     }
 }
